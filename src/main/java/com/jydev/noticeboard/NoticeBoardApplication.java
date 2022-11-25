550475cba26a8b2217e5d45045d043e6f59d5312
jaeyoung <kjy0302014@gmail.com>
2022-11-25 21:52:30 +0900

    Initial commit

---
+++ src/main/java/com/jydev/noticeboard/NoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jydev.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardApplication.class, args);
+	}
+
+}
