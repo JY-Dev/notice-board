a8a60fb2d2d96988edf776be3719962c6dca5b56
jaeyoung <kjy0302014@gmail.com>
2022-11-28 19:58:49 +0900

    UserRegisterRequest Model 생성

---
+++ src/main/java/com/jydev/noticeboard/user/model/request/UserRegisterRequest.java
@@ -0,0 +1,24 @@
+package com.jydev.noticeboard.user.model.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegisterRequest {
+    @NotBlank
+    private String nickname;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String id;
+
+    @NotBlank
+    @Size(min = 8,max = 12)
+    private String password;
+}
