898cec6966403b60d3c6858d82725771dda929ce
jaeyoung <kjy0302014@gmail.com>
2022-11-28 20:28:58 +0900

    UserController register 화면으로 가는 로직 추가

--- src/main/java/com/jydev/noticeboard/user/UserController.java
+++ src/main/java/com/jydev/noticeboard/user/UserController.java
@@ -1,10 +1,15 @@
 package com.jydev.noticeboard.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @GetMapping("/register")
+    public String registerForm(){
+        return "user/register";
+    }
 }
