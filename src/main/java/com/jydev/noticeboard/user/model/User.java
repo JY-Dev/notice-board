dfcd8b697fac5748b4adac6095faa1262c9e613b
jaeyoung <kjy0302014@gmail.com>
2022-11-29 21:21:17 +0900

    User EqualsHashCode 추가

--- src/main/java/com/jydev/noticeboard/user/model/User.java
+++ src/main/java/com/jydev/noticeboard/user/model/User.java
@@ -1,10 +1,12 @@
 package com.jydev.noticeboard.user.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class User {
     private String email;
     private String nickname;
