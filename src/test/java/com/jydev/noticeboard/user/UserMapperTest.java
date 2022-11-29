bc59640bd3e33f2d98b458f062114809e61bfad2
jaeyoung <kjy0302014@gmail.com>
2022-11-29 21:41:49 +0900

    UserMapperTest userEntityToUserTest 작성

--- src/test/java/com/jydev/noticeboard/user/UserMapperTest.java
+++ src/test/java/com/jydev/noticeboard/user/UserMapperTest.java
@@ -1,6 +1,7 @@
 package com.jydev.noticeboard.user;
 
 import com.jydev.noticeboard.user.model.Mapper.UserMapper;
+import com.jydev.noticeboard.user.model.User;
 import com.jydev.noticeboard.user.model.entity.UserEntity;
 import com.jydev.noticeboard.user.model.request.UserRegisterRequest;
 import com.jydev.noticeboard.user.util.UserMockFactory;
@@ -8,15 +9,23 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserMapperTest {
-    UserMapper userMapper = new UserMapper();
-
+    private final UserMapper userMapper = new UserMapper();
+    private final String id = "id";
+    private final String pw = "pw";
     @Test
     void userRegisterRequestToEntityTest(){
-        String id = "id";
-        String pw = "pw";
+
         UserRegisterRequest request = UserMockFactory.makeUserRegisterRequest(id,pw);
         UserEntity userEntity = UserMockFactory.makeUserEntity(id,pw);
         UserEntity result = userMapper.toEntity(request);
         Assertions.assertThat(userEntity).isEqualTo(result);
+    }
+
+    @Test
+    void userEntityToUserTest(){
+        UserEntity userEntity = UserMockFactory.makeUserEntity(id,pw);
+        User result = userMapper.toUser(userEntity);
+        User user = UserMockFactory.makeUser(id);
+        Assertions.assertThat(result).isEqualTo(user);
     }
 }
