776f85ed5dd8387ac1e70e992dbce3c513308c87
jaeyoung <kjy0302014@gmail.com>
2022-11-29 20:49:27 +0900

    UserMapper UserEntity toUser 함수 추가

--- src/main/java/com/jydev/noticeboard/user/model/Mapper/UserMapper.java
+++ src/main/java/com/jydev/noticeboard/user/model/Mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.jydev.noticeboard.user.model.Mapper;
 
 
+import com.jydev.noticeboard.user.model.User;
 import com.jydev.noticeboard.user.model.UserRole;
 import com.jydev.noticeboard.user.model.entity.UserEntity;
 import com.jydev.noticeboard.user.model.request.UserRegisterRequest;
@@ -10,5 +11,8 @@ import org.springframework.stereotype.Component;
 public class UserMapper {
     public UserEntity toEntity(UserRegisterRequest request){
         return new UserEntity(request.getEmail(),request.getNickname(),request.getId(),request.getPassword(), UserRole.COMMON);
+    }
+    public User toUser(UserEntity userEntity){
+        return new User(userEntity.getEmail(),userEntity.getNickname(),userEntity.getId(),userEntity.getRole());
     }
 }
