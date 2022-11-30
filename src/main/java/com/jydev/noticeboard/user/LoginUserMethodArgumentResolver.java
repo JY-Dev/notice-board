ac3fc0e74d3c6ff1152d8c69920dc52be54f9644
jaeyoung <kjy0302014@gmail.com>
2022-11-30 20:42:03 +0900

    LoginUserMethodArgumentResolver resolveArgument 로직 수정

--- src/main/java/com/jydev/noticeboard/user/LoginUserMethodArgumentResolver.java
+++ src/main/java/com/jydev/noticeboard/user/LoginUserMethodArgumentResolver.java
@@ -2,6 +2,8 @@ package com.jydev.noticeboard.user;
 
 import com.jydev.noticeboard.user.model.User;
 import com.jydev.noticeboard.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -21,8 +23,16 @@ public class LoginUserMethodArgumentResolver implements HandlerMethodArgumentRes
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        User user = userService.getLoginUserById(webRequest.getSessionId()).orElse(null);
-        webRequest.setAttribute("user",user,0);
-        return user;
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if(session == null)
+            return null;
+        else{
+            User user = userService.getLoginUserById(session.getId()).orElse(null);
+            if(user == null)
+                return null;
+            webRequest.setAttribute("user",user,0);
+            return user;
+        }
     }
 }
