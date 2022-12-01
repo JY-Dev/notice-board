eace2a65480639142be6dd0869cca8b15d9cb47f
jaeyoung <kjy0302014@gmail.com>
2022-12-01 17:34:14 +0900

    BlockLoginUserInterceptor 생성 및 등록

--- src/main/java/com/jydev/noticeboard/WebConfig.java
+++ src/main/java/com/jydev/noticeboard/WebConfig.java
@@ -1,10 +1,12 @@
 package com.jydev.noticeboard;
 
+import com.jydev.noticeboard.interceptor.BlockLoginUserInterceptor;
 import com.jydev.noticeboard.user.LoginUserMethodArgumentResolver;
 import com.jydev.noticeboard.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserMethodArgumentResolver(userService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new BlockLoginUserInterceptor(userService))
+                .order(2)
+                .addPathPatterns("/user/login","/user/register");
     }
 }
