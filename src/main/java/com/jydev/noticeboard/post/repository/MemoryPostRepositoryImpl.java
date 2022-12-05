a09433177672b75540ffd222029102a63de2db40
jaeyoung <kjy0302014@gmail.com>
2022-12-05 23:13:42 +0900

    MemoryPostRepositoryImpl 생성 및 PostRepository savePost registerUser 매개변수 추가

---
+++ src/main/java/com/jydev/noticeboard/post/repository/MemoryPostRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.jydev.noticeboard.post.repository;
+
+import com.jydev.noticeboard.post.model.entity.PostEntity;
+import com.jydev.noticeboard.post.model.request.PostRequest;
+import com.jydev.noticeboard.user.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RequiredArgsConstructor
+@Repository
+public class MemoryPostRepositoryImpl implements PostRepository {
+    private final Map<Long, PostEntity> storePost = new HashMap<>();
+    private final AtomicLong idCnt = new AtomicLong();
+
+    @Override
+    public PostEntity savePost(PostRequest request, User registerUser) {
+        long postId = idCnt.get();
+        idCnt.set(postId+1);
+        PostEntity postEntity = new PostEntity(postId, request.getTitle(), request.getContent(), registerUser);
+        storePost.put(postId,postEntity);
+        return postEntity;
+    }
+
+    @Override
+    public PostEntity findPostById(Long postId) {
+        return storePost.get(postId);
+    }
+
+    @Override
+    public void deletePostById(Long postId) {
+        storePost.remove(postId);
+    }
+
+}
--- src/main/java/com/jydev/noticeboard/post/repository/PostRepository.java
+++ src/main/java/com/jydev/noticeboard/post/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.jydev.noticeboard.post.repository;
 
 import com.jydev.noticeboard.post.model.entity.PostEntity;
+import com.jydev.noticeboard.post.model.request.PostRequest;
+import com.jydev.noticeboard.user.model.User;
 
 public interface PostRepository {
-    PostEntity savePost();
+    PostEntity savePost(PostRequest request, User registerUser);
     PostEntity findPostById(Long postId);
     void deletePostById(Long postId);
 }
