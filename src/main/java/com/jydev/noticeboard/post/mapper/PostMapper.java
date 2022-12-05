b4d15da9c5e2bed87eac920e86f0414fe9a59486
jaeyoung <kjy0302014@gmail.com>
2022-12-05 23:24:04 +0900

    PostMapper 생성 및 PostEntity toPost 함수 작성

---
+++ src/main/java/com/jydev/noticeboard/post/mapper/PostMapper.java
@@ -0,0 +1,14 @@
+package com.jydev.noticeboard.post.mapper;
+
+import com.jydev.noticeboard.post.model.Post;
+import com.jydev.noticeboard.post.model.entity.PostEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+    public Post toPost(PostEntity postEntity) {
+        return new Post(postEntity.getId(), postEntity.getRegisterUser().getNickname(),
+                postEntity.getRegisterUser().getId(),
+                postEntity.getTitle(), postEntity.getContent());
+    }
+}
