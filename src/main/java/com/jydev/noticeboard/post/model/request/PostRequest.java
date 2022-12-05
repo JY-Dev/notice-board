ebbcd0c8ca42fb1fcff2d9aa6a959bfec3f83051
jaeyoung <kjy0302014@gmail.com>
2022-12-05 22:00:53 +0900

    PostRequest 생성

---
+++ src/main/java/com/jydev/noticeboard/post/model/request/PostRequest.java
@@ -0,0 +1,17 @@
+package com.jydev.noticeboard.post.model.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostRequest {
+    @NotBlank
+    String title;
+    String content;
+    @NotBlank
+    String userId;
+}
