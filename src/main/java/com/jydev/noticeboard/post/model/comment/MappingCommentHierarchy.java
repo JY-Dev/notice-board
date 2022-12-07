e7423e27950727d14e7d5ce7f3413cc5344bd0f3
jaeyoung <kjy0302014@gmail.com>
2022-12-07 22:17:03 +0900

    CommentResponse MappingCommentHierarchy Getter AllArgsConstructor 추가

--- src/main/java/com/jydev/noticeboard/post/model/comment/MappingCommentHierarchy.java
+++ src/main/java/com/jydev/noticeboard/post/model/comment/MappingCommentHierarchy.java
@@ -1,8 +1,13 @@
 package com.jydev.noticeboard.post.model.comment;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class MappingCommentHierarchy {
-    Comment parentComment;
-    List<Comment> childComment;
+    private Comment parentComment;
+    private List<Comment> childComment;
 }
--- src/main/java/com/jydev/noticeboard/post/model/comment/response/CommentResponse.java
+++ src/main/java/com/jydev/noticeboard/post/model/comment/response/CommentResponse.java
@@ -1,10 +1,14 @@
 package com.jydev.noticeboard.post.model.comment.response;
 
 import com.jydev.noticeboard.post.model.comment.MappingCommentHierarchy;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class CommentResponse {
-    Long postId;
-    List<MappingCommentHierarchy> comments;
+    private Long postId;
+    private List<MappingCommentHierarchy> comments;
 }
